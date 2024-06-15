83b81c7c8496f02ad0fcac56ae7b103b2b8577fe
Tien Dat Pham <dat20036@gmail.com>
2024-06-15 09:54:35 +0700

    Rewrite bootstrap code & add first API endpoint.

---
+++ MSOC.Backend/Api/TestApi.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MSOC.Backend.Api;
+
+[ApiController]
+[Route("[controller]")]
+public class TestApi : ControllerBase
+{
+    [HttpGet("/healthcheck")]
+    public IActionResult HealthCheck()
+    {
+        return Ok("I am OK");
+    }
+}
