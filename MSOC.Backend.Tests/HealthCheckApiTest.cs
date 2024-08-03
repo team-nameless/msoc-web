42776fb0555fc5d8f454bf271a30d1a62625ac6d
Tien Dat Pham <dat20036@gmail.com>
2024-08-03 14:24:24 +0700

    Add initial tests.

---
+++ MSOC.Backend.Tests/HealthCheckApiTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MSOC.Backend.Controller;
+using Xunit;
+
+namespace MSOC.Backend.Tests;
+
+public class HealthCheckApiTest
+{
+    /// <summary>
+    ///     Smoke test(n):
+    ///         preliminary testing or sanity testing to reveal simple failures severe enough to,
+    ///         for example, reject a prospective software release.
+    /// </summary>
+    [Fact]
+    public void ApiSmokeTest()
+    {
+        var healthCheckController = new TestController();
+        var result = healthCheckController.HealthCheck();
+        
+        Assert.NotNull(result);
+        Assert.Multiple(() =>
+        {
+            Assert.IsType<OkObjectResult>(result);
+            Assert.StrictEqual((result as OkObjectResult)?.Value, "I am OK");
+        });
+    }
+}
--- MSOC.Backend/Program.cs
+++ MSOC.Backend/Program.cs
@@ -67,3 +67,5 @@ app
 
 app.MapControllers();
 app.Run();
+
+public partial class Program {}
