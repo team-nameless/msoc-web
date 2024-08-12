e07eb0aad89ce15f7b588457f469d0656d40d80a
Tien Dat Pham <dat20036@gmail.com>
2024-08-12 09:15:16 +0700

    Move database service setup code to DatabaseService.cs

--- MSOC.Backend/Program.cs
+++ MSOC.Backend/Program.cs
@@ -1,8 +1,6 @@
 using System.Globalization;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.Data.Sqlite;
-using Microsoft.EntityFrameworkCore;
 using MSOC.Backend.Service;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -19,18 +17,7 @@ builder.Services.AddSignalR();
 // Add crapwares to the controller    
 builder.Services
     .AddSingleton<MaimaiInquiryService>()
-    .AddDbContext<DatabaseService>(x =>
-    {
-        x
-#if DEBUG
-            .EnableSensitiveDataLogging()
-            .EnableDetailedErrors()
-#endif
-            .UseSqlite(new SqliteConnection(
-                $"Data Source=MSOC.db;" +
-                $"Password={builder.Configuration.GetValue<string>("Database:PASSWORD")};"
-            ));
-    })
+    .AddDbContext<DatabaseService>()
     .AddRouting()
     .AddEndpointsApiExplorer()
     .AddHttpContextAccessor()
--- MSOC.Backend/Service/DatabaseService.cs
+++ MSOC.Backend/Service/DatabaseService.cs
@@ -1,15 +1,38 @@
+using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using MSOC.Backend.Database.Models;
 
 namespace MSOC.Backend.Service;
 
+/// <summary>
+///     A database service, for interacting with databases.
+///     Meant to be used with DI.
+/// </summary>
 public class DatabaseService : DbContext
 {
-    public DatabaseService(DbContextOptions<DatabaseService> options) : base(options)
+    private readonly IConfiguration _configuration;
+
+    public DatabaseService(IConfiguration configuration)
     {
+        _configuration = configuration;
     }
 
     public DbSet<Player> Players { get; set; }
     public DbSet<Team> Teams { get; set; }
     public DbSet<School> Schools { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(
+            optionsBuilder
+#if DEBUG
+                .EnableDetailedErrors()
+                .EnableSensitiveDataLogging()
+#endif
+                .UseSqlite(new SqliteConnection(
+                    $"Data Source=MSOC.db;" +
+                    $"Password={_configuration.GetValue<string>("Database:PASSWORD")};"
+                ))
+        );
+    }
 }
