a6bffcad98bf4c4bb54300a47047c10c88c615aa
Tien Dat Pham <dat20036@gmail.com>
2024-08-17 10:36:56 +0700

    Test only the maimai inquiry service, not the entire API set.
    Since it is the *core* of the endpoint(s). The API only pours the result of the function call to the response text.

---
+++ MSOC.Backend.Tests/Unit/MaimaiInquiryServiceTest.cs
@@ -0,0 +1,45 @@
+﻿using AngleSharp.Dom;
+using MSOC.Backend.Service;
+
+namespace MSOC.Backend.Tests.Unit;
+
+public class MaimaiInquiryServiceTest
+{
+    private MaimaiInquiryService _maimai;
+    
+    public MaimaiInquiryServiceTest(MaimaiInquiryService maimai)
+    {
+        _maimai = maimai;
+    }
+
+    [Theory]
+    [InlineData(1234)]
+    [InlineData(69420)]
+    [InlineData(177013)]
+    public async Task InvalidFriendCodeTest(ulong friendCode)
+    {
+        IElement[] result = await _maimai.PerformFriendCodeLookupAsync(friendCode);
+
+        Assert.StrictEqual(0, result.Length);
+    }
+
+    [Theory]
+    [InlineData(9051555929120)]
+    [InlineData(8095773611588)]
+    [InlineData(9020119099087)]
+    public async Task ValidFamiliarFriendCodeTest(ulong friendCode)
+    {
+        IElement[] result = await _maimai.PerformFriendCodeLookupAsync(friendCode);
+
+        Assert.StrictEqual(2, result.Length);
+    }
+
+    [Theory]
+    [InlineData(8069933165057)]
+    public async Task ValidStrangerFriendCodeTest(ulong friendCode)
+    {
+        IElement[] result = await _maimai.PerformFriendCodeLookupAsync(friendCode);
+
+        Assert.StrictEqual(2, result.Length);
+    }
+}
