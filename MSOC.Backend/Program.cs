ee28e58928c6cf2fc36983f62df15f8448c47962
Tien Dat Pham <dat20036@gmail.com>
2024-08-29 19:18:09 +0700

    We use JSON for config file now.

--- MSOC.Backend/Program.cs
+++ MSOC.Backend/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add config file
 builder.Configuration
-    .AddIniFile("config.ini")
+    .AddJsonFile("config.json")
     .Build();
 
 // :thinking:
