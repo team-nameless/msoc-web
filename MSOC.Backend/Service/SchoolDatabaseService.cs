c8a87c1a6ee46634d9c72478f276a6f66555d3b2
Tien Dat Pham <dat20036@gmail.com>
2024-08-29 19:21:49 +0700

    Somehow "Search All Solution" is not *that* reliable.

--- MSOC.Backend/Service/SchoolDatabaseService.cs
+++ MSOC.Backend/Service/SchoolDatabaseService.cs
@@ -24,7 +24,6 @@ public class SchoolDatabaseService : DbContext
                 .EnableDetailedErrors()
                 .EnableSensitiveDataLogging()
 #endif
-                .UseSqlite(new SqliteConnection("Filename=schools.db;"))
         );
     }
 
--- MSOC.Backend/Service/TrackDatabaseService.cs
+++ MSOC.Backend/Service/TrackDatabaseService.cs
@@ -24,7 +24,6 @@ public class TrackDatabaseService : DbContext
                 .EnableDetailedErrors()
                 .EnableSensitiveDataLogging()
 #endif
-                .UseSqlite(new SqliteConnection("Filename=tracks.db;"))
         );
     }
 
