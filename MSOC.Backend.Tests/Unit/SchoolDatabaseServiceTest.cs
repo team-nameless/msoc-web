5e7f90df59e740124c2e80a5800a9c8feca79915
Tien Dat Pham <dat20036@gmail.com>
2024-08-30 08:34:42 +0700

    Add school.db database file and test suite.

---
+++ MSOC.Backend.Tests/Unit/SchoolDatabaseServiceTest.cs
@@ -0,0 +1,38 @@
+using MSOC.Backend.Service;
+using Xunit.Abstractions;
+using Xunit.Microsoft.DependencyInjection.Abstracts;
+
+namespace MSOC.Backend.Tests.Unit;
+
+public class SchoolDatabaseServiceTest(ITestOutputHelper testOutputHelper, BackendTestBedFixture fixture) 
+    : TestBed<BackendTestBedFixture>(testOutputHelper, fixture)
+{
+    [Fact]
+    public void DatabaseInjectionWorks()
+    {
+        var schoolDatabase = _fixture.GetService<SchoolDatabaseService>(_testOutputHelper)!;
+        schoolDatabase.Database.EnsureCreated();
+        
+        Assert.NotNull(schoolDatabase);
+    }
+    
+    [Fact]
+    public void AllTablesExists()
+    {        
+        var schoolDatabase = _fixture.GetService<SchoolDatabaseService>(_testOutputHelper)!;
+        schoolDatabase.Database.EnsureCreated();
+        
+        var _ = schoolDatabase.Schools;
+        
+        Assert.True(true);
+    }
+    
+    [Fact]
+    public void AllEntriesExists()
+    {
+        var schoolDatabase = _fixture.GetService<SchoolDatabaseService>(_testOutputHelper)!;
+        schoolDatabase.Database.EnsureCreated();
+        
+        Assert.StrictEqual(256, schoolDatabase.Schools.Count());
+    }
+}
